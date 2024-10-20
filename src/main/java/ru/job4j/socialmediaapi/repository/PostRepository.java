3a2a8244d97f7499a213589f5771a61c0fb90cce
XLoganxmenX <loganxmen97@gmail.com>
2024-10-20 13:17:03 +0900

    extend PostRepository by methods: findByUser, findByCreatedBetween, findByOrderByCreatedDesc.

--- src/main/java/ru/job4j/socialmediaapi/repository/PostRepository.java
+++ src/main/java/ru/job4j/socialmediaapi/repository/PostRepository.java
@@ -1,7 +1,19 @@
 package ru.job4j.socialmediaapi.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.ListCrudRepository;
 import ru.job4j.socialmediaapi.model.Post;
+import ru.job4j.socialmediaapi.model.User;
+
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface PostRepository extends ListCrudRepository<Post, Integer> {
+    List<Post> findByUser(User user);
+
+    List<Post> findByCreatedBetween(LocalDateTime startDate, LocalDateTime endDate);
+
+    Page<Post> findByOrderByCreatedDesc(Pageable pageable);
 }
--- src/test/java/ru/job4j/socialmediaapi/repository/PostRepositoryTest.java
+++ src/test/java/ru/job4j/socialmediaapi/repository/PostRepositoryTest.java
@@ -1,11 +1,11 @@
 package ru.job4j.socialmediaapi.repository;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 import ru.job4j.socialmediaapi.model.Post;
 import ru.job4j.socialmediaapi.model.User;
 
@@ -95,5 +95,72 @@ class PostRepositoryTest {
     public void whenFindAllPostsNotExistThenGetEmptyList() {
         var actualPosts = postRepository.findAll();
         assertThat(actualPosts).isEmpty();
+    }
+
+    @Test
+    public void whenFindPostByUserThenGetUsersPostList() {
+        var user = userRepository.save(
+                new User(0, "user1", "test1@test.com", "test1", "UTC", Set.of()));
+        var userPost1 = postRepository.save(
+                new Post(0, "title1", "description1", user, LocalDateTime.now(), List.of()));
+        var userPost2 = postRepository.save(
+                new Post(0, "title2", "description2", user, LocalDateTime.now(), List.of()));
+        var expectedPosts = List.of(userPost1, userPost2);
+
+        var anotherUser = userRepository.save(
+                new User(0, "another user", "anothertest@test.com", "another test", "UTC", Set.of()));
+        var anotherUserPost = postRepository.save(
+                new Post(0, "another title", "another description", anotherUser, LocalDateTime.now(), List.of()));
+
+        var actualUserPosts = postRepository.findByUser(user);
+        assertThat(actualUserPosts).hasSize(2);
+        assertThat(actualUserPosts).usingRecursiveComparison()
+                .ignoringFields("user", "created", "files")
+                .isEqualTo(expectedPosts);
+    }
+
+    @Test
+    public void whenFindPostByCreatedBetweenThenGetMonthPostList() {
+        var today = LocalDateTime.now();
+        var user = userRepository.save(
+                new User(0, "user1", "test1@test.com", "test1", "UTC", Set.of()));
+        var userMatchingPost1 = postRepository.save(
+                new Post(0, "title1", "description1", user, today.minusDays(5), List.of()));
+        var userMatchingPost2 = postRepository.save(
+                new Post(0, "title2", "description2", user, today.minusDays(29), List.of()));
+        var userNotMatchingPost3 = postRepository.save(
+                new Post(0, "title3", "description3", user, today.minusMonths(6), List.of()));
+
+        var anotherUser = userRepository.save(
+                new User(0, "another user", "anothertest@test.com", "another test", "UTC", Set.of()));
+        var anotherUserMatchingPost = postRepository.save(
+                new Post(0, "another title", "another description", anotherUser, today.minusHours(1), List.of()));
+        var expectedPosts = List.of(userMatchingPost1, userMatchingPost2, anotherUserMatchingPost);
+
+        var actualUserPosts = postRepository.findByCreatedBetween(today.minusDays(30), today);
+        assertThat(actualUserPosts).hasSize(3);
+        assertThat(actualUserPosts).usingRecursiveComparison()
+                .ignoringFields("user", "created", "files")
+                .isEqualTo(expectedPosts);
+    }
+
+    @Test
+    public void whenFindPostOrderByCreatedDescThenGetDescOrderPostList() {
+        var today = LocalDateTime.now();
+        var user = userRepository.save(
+                new User(0, "user1", "test1@test.com", "test1", "UTC", Set.of()));
+        var userPost1 = postRepository.save(
+                new Post(0, "title1", "description1", user, today.minusDays(5), List.of()));
+        var userPost2 = postRepository.save(
+                new Post(0, "title2", "description2", user, today.minusHours(2), List.of()));
+        var userPost3 = postRepository.save(
+                new Post(0, "title3", "description3", user, today.minusMonths(6), List.of()));
+        var expectedPosts = List.of(userPost2, userPost1, userPost3);
+
+        var actualUserPosts = postRepository.findByOrderByCreatedDesc(PageRequest.of(0, 3)).getContent();
+        assertThat(actualUserPosts).usingRecursiveComparison()
+                .ignoringFields("user", "created", "files")
+                .isEqualTo(expectedPosts);
+        assertThat(actualUserPosts).containsExactlyElementsOf(expectedPosts);
     }
 }
